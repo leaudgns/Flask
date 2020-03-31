774c4afa29721ccacea267f52562a0b0481b5df8
leaudgns <62051982+leaudgns@users.noreply.github.com>
2020-03-31 18:22:47 +0900

    python flask 4.2

--- hello.py
+++ hello.py
@@ -1,16 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("SuperScrapper")
 
 
 @app.route("/")
 def home():
-    return "Hello! Welcome to mi casa"
+    return render_template("potato.html")
 
 
-@app.route("/contact")
-def contact():
-    return "Contact"
+@app.route("/<username>")
+def potato(username):
+    return f"Hello {username} how are you doing"
 
 
 app.run()
