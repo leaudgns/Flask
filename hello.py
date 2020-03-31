3f08ac2c6c9f16930c85ed16197d4a1faa126464
leaudgns <62051982+leaudgns@users.noreply.github.com>
2020-03-31 17:47:01 +0900

    python flask 4.1

---
+++ hello.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask("SuperScrapper")
+
+
+@app.route("/")
+def home():
+    return "Hello! Welcome to mi casa"
+
+
+@app.route("/contact")
+def contact():
+    return "Contact"
+
+
+app.run()
